e5a698a1c09b1950adf062a877fa9f75c9212d82
Monica Bello <mc_bello@bellsouth.net>
2017-07-05 18:37:28 -0400

    uploading pictures and styling profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :pictures
- before_save :delete_picture, if: ->{ remove_pictre == '1' && !picture_updated_at_changed? }
-
+ 
   private
 
   def delete_avatar
